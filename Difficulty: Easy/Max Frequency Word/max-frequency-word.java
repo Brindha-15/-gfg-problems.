ea718a7dc888524581c5bd426134760321254c4a
Brindha-15 <brindhu1503@gmail.com>
2026-09-10 23:51:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Frequency Word/max-frequency-word.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String maximumFrequency(String s) {
+        String[] words = s.split(" ");
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        String ans = words[0];
+        int max = map.get(words[0]);
+        for (String word : words) {
+            if (map.get(word) > max) {
+                max = map.get(word);
+                ans = word;
+            }
+        }
+        return ans + " " + max;
+    }
+}
